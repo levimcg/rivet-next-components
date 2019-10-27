70fd24ad1d9493a03c881c884012c7d5a7219c27
lmcgrana <lmcgrana@iu.edu>
2019-10-27 13:34:12 -0400

    Added local nav

--- src/js/rivet-2-prototype-components.js
+++ src/js/rivet-2-prototype-components.js
@@ -7,17 +7,39 @@ function init() {
   const globalMenuToggle = document.querySelector('[data-global-menu-toggle]');
   globalMenu.setAttribute('hidden', '');
   
+  const localMenu = document.getElementById('local-menu');
+  const localMenuToggle = document.querySelector('[data-local-menu-toggle]');
+  localMenu.setAttribute('hidden', '');
+  
+  function openMenu(menu, toggle) {
+    menu.removeAttribute('hidden');
+    toggle.setAttribute('aria-expanded', 'true');
+  }
+  
+  function closeMenu(menu, toggle) {
+    menu.setAttribute('hidden', '');
+    toggle.setAttribute('aria-expanded', 'false');
+  }
+  
   // Listen for clicks and toggle
   globalMenuToggle.addEventListener('click', () => {
     let isExpanded = globalMenuToggle.getAttribute('aria-expanded');
     if (isExpanded == 'false') {
-      globalMenu.removeAttribute('hidden');
-      globalMenuToggle.setAttribute('aria-expanded', 'true');
+      openMenu(globalMenu, globalMenuToggle);
     } else {
-      globalMenu.setAttribute('hidden', '');
-      globalMenuToggle.setAttribute('aria-expanded', 'false');
+      closeMenu(globalMenu, globalMenuToggle);
     }
-  })
+  });
+  
+  // Listen for clicks and toggle
+  localMenuToggle.addEventListener('click', () => {
+    let isExpanded = localMenuToggle.getAttribute('aria-expanded');
+    if (isExpanded == 'false') {
+      openMenu(localMenu, localMenuToggle);
+    } else {
+      closeMenu(localMenu, localMenuToggle);
+    }
+  });
 }
 
 export default { NavMenu, init };
