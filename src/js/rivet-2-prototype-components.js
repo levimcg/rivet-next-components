d0d15f89fc156b67cb59376775e1aac785c8fa4b
lmcgrana <lmcgrana@iu.edu>
2019-10-27 14:31:09 -0400

    Reworked mobile menu toggles

--- src/js/rivet-2-prototype-components.js
+++ src/js/rivet-2-prototype-components.js
@@ -2,15 +2,6 @@ import NavMenu from './components/NavMenu';
 
 // This is lame. Needs refactored into something reusable for the search.
 function init() {
-  // Initial set up
-  const globalMenu = document.getElementById('global-menu');
-  const globalMenuToggle = document.querySelector('[data-global-menu-toggle]');
-  globalMenu.setAttribute('hidden', '');
-  
-  const localMenu = document.getElementById('local-menu');
-  const localMenuToggle = document.querySelector('[data-local-menu-toggle]');
-  localMenu.setAttribute('hidden', '');
-  
   function openMenu(menu, toggle) {
     menu.removeAttribute('hidden');
     toggle.setAttribute('aria-expanded', 'true');
@@ -21,6 +12,10 @@ function init() {
     toggle.setAttribute('aria-expanded', 'false');
   }
   
+  const globalMenu = document.getElementById('global-menu');
+  const globalMenuToggle = document.querySelector('[data-global-menu-toggle]');
+  
+  globalMenu.setAttribute('hidden', '');
   // Listen for clicks and toggle
   globalMenuToggle.addEventListener('click', () => {
     let isExpanded = globalMenuToggle.getAttribute('aria-expanded');
@@ -31,15 +26,21 @@ function init() {
     }
   });
   
-  // Listen for clicks and toggle
-  localMenuToggle.addEventListener('click', () => {
-    let isExpanded = localMenuToggle.getAttribute('aria-expanded');
-    if (isExpanded == 'false') {
-      openMenu(localMenu, localMenuToggle);
-    } else {
-      closeMenu(localMenu, localMenuToggle);
-    }
-  });
+  const localMenu = document.getElementById('local-menu');
+  const localMenuToggle = document.querySelector('[data-local-menu-toggle]');
+  
+  if (localMenu) {
+    localMenu.setAttribute('hidden', '');
+    // Listen for clicks and toggle
+    localMenuToggle.addEventListener('click', () => {
+      let isExpanded = localMenuToggle.getAttribute('aria-expanded');
+      if (isExpanded == 'false') {
+        openMenu(localMenu, localMenuToggle);
+      } else {
+        closeMenu(localMenu, localMenuToggle);
+      }
+    });
+  }
 }
 
 export default { NavMenu, init };
