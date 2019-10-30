cdd173fc3bfd1a4be045335340e135e314dccc7a
lmcgrana <lmcgrana@iu.edu>
2019-10-30 14:19:24 -0400

    Adds generic toggle

--- src/js/rivet-2-prototype-components.js
+++ src/js/rivet-2-prototype-components.js
@@ -1,23 +1,45 @@
 // TODO: Refactor this into something coherent
 function init() {
-  console.log('All good to go!');
+  // console.log('All good to go!');
 }
 
 function toggle() {
-  function handleClick() {
+  let currentElement = null;
+
+  function handleClick(event) {
     const toggleTrigger = event.target.closest('[data-toggle]');
-    if (!toggleTrigger) return;
+    if (!toggleTrigger) {
+      if(currentElement) {
+        const currentToggle = document.querySelector(`[data-toggle="${currentElement.id}"]`);
+        hideElement(currentToggle, currentElement);
+        return;
+      }
+      return;
+    }
     
     const toggleId = toggleTrigger.dataset.toggle;
     const elementToToggle = document.getElementById(toggleId);
+    // If the click happened inside the toggled element, bail.
+    if (event.target.closest('#' + toggleId)) return;
     
-    if (toggleTrigger.getAttribute('aria-expanded') == 'false') {
-      toggleTrigger.setAttribute('aria-expanded', 'true');
-      elementToToggle.classList.add('rvt-display-block');
-    } else {
-      toggleTrigger.setAttribute('aria-expanded', 'false');
-      elementToToggle.classList.remove('rvt-display-block')
-    }
+    const isExpanded = toggleTrigger.getAttribute('aria-expanded');
+    
+    isExpanded == 'false' ?
+      showElement(toggleTrigger, elementToToggle) :
+      hideElement(toggleTrigger, elementToToggle);
+  }
+  
+  function showElement(trigger, element) {
+    trigger.setAttribute('aria-expanded', 'true');
+    element.classList.add('rvt-display-block');
+    currentElement = element;
+    console.log(currentElement);
+  }
+  
+  function hideElement(trigger, element) {
+    trigger.setAttribute('aria-expanded', 'false');
+    element.classList.remove('rvt-display-block');
+    currentElement = null;
   }
   
   document.addEventListener('click', handleClick, false)
