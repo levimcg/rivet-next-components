cb9347cf88b15a9474f91fa70eacee29aae0e653
lmcgrana <lmcgrana@iu.edu>
2019-10-30 13:17:39 -0400

    Stubs out generic toggle

--- src/js/rivet-2-prototype-components.js
+++ src/js/rivet-2-prototype-components.js
@@ -1,60 +1,26 @@
 // TODO: Refactor this into something coherent
 function init() {
-  function openMenu(menu, toggle) {
-    menu.removeAttribute('hidden');
-    toggle.setAttribute('aria-expanded', 'true');
-  }
-  
-  function closeMenu(menu, toggle) {
-    menu.setAttribute('hidden', '');
-    toggle.setAttribute('aria-expanded', 'false');
-  }
-  
-  const globalMenu = document.getElementById('global-menu');
-  const globalMenuToggle = document.querySelector('[data-global-menu-toggle]');
-  
-  globalMenu.setAttribute('hidden', '');
-  // Listen for clicks and toggle
-  globalMenuToggle.addEventListener('click', () => {
-    let isExpanded = globalMenuToggle.getAttribute('aria-expanded');
-    if (isExpanded == 'false') {
-      openMenu(globalMenu, globalMenuToggle);
-    } else {
-      closeMenu(globalMenu, globalMenuToggle);
-    }
-  });
-  
-  const localMenu = document.getElementById('local-menu');
-  const localMenuToggle = document.querySelector('[data-local-menu-toggle]');
-  
-  if (localMenu) {
-    localMenu.setAttribute('hidden', '');
-    // Listen for clicks and toggle
-    localMenuToggle.addEventListener('click', () => {
-      let isExpanded = localMenuToggle.getAttribute('aria-expanded');
-      if (isExpanded == 'false') {
-        openMenu(localMenu, localMenuToggle);
-      } else {
-        closeMenu(localMenu, localMenuToggle);
-      }
-    });
-  }
-  
-  // const secondaryMenu = document.getElementById('secondary-menu');
-  // const secondaryToggle = document.getElementById('secondary-menu-toggle');
-
-  // if (secondaryMenu) {
-  //   secondaryMenu.setAttribute('hidden', '');
-  //   // Listen for clicks and toggle
-  //   secondaryToggle.addEventListener('click', () => {
-  //     let isExpanded = secondaryToggle.getAttribute('aria-expanded');
-  //     if (isExpanded == 'false') {
-  //       openMenu(secondaryMenu, secondaryToggle);
-  //     } else {
-  //       closeMenu(secondaryMenu, secondaryToggle);
-  //     }
-  //   });
-  // }
+  console.log('All good to go!');
 }
 
-export default { init };
+function toggle() {
+  function handleClick() {
+    const toggleTrigger = event.target.closest('[data-toggle]');
+    if (!toggleTrigger) return;
+    
+    const toggleId = toggleTrigger.dataset.toggle;
+    const elementToToggle = document.getElementById(toggleId);
+    
+    if (toggleTrigger.getAttribute('aria-expanded') == 'false') {
+      toggleTrigger.setAttribute('aria-expanded', 'true');
+      elementToToggle.classList.add('rvt-display-block');
+    } else {
+      toggleTrigger.setAttribute('aria-expanded', 'false');
+      elementToToggle.classList.remove('rvt-display-block')
+    }
+  }
+  
+  document.addEventListener('click', handleClick, false)
+}
+
+export default { init, toggle };
