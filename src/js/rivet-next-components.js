1b29f5d556b23a46e58f841ec100a0bb8eedfd86
lmcgrana <lmcgrana@iu.edu>
2019-10-31 16:18:07 -0400

    Added comments to js file

--- src/js/rivet-next-components.js
+++ src/js/rivet-next-components.js
@@ -1,3 +1,9 @@
+/**
+ * NOTE: These polyfills are included by default in Rivet's JS file
+ * (rivet.js). But if the toggle element needs used on it's own for
+ * any reason, uncomment the two lines below and rebuild file.
+ */
+
 import './polyfills/closest';
 import './polyfills/CustomEvent.js';
 import toggle from './components/toggle';
