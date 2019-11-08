d666b7bcdfc2d13dff310356c39c0ded4f3b6d07
lmcgrana <lmcgrana@iu.edu>
2019-11-08 09:47:40 -0500

    Updates

--- js/rivet-next-components.js
+++ js/rivet-next-components.js
@@ -426,11 +426,11 @@ var RivetNext = (function () {
       context = document;
     }
 
-    menuToggles.forEach(function (menuToggle) {
+    Array.prototype.slice.call(menuToggles).forEach(function (menuToggle) {
       menuToggle.setAttribute('aria-expanded', 'false');
       menuToggle.setAttribute('aria-haspopup', 'true');
     });
-    childMenus.forEach(function (childMenu) {
+    Array.prototype.slice.call(childMenus).forEach(function (childMenu) {
       childMenu.setAttribute('aria-hidden', 'true');
     }); // Destroy any currently initialized dropdowns
 
